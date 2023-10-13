439fefb5fef14f856ede2aafdc22b1c2ddbeaebb
ranaslait <125487372+ranaslait@users.noreply.github.com>
2023-10-13 17:21:00 +0300

    signup login about us

---
+++ Views/Pages/aboutus.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" />
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://www.w3schools.com/lib/w3-theme-black.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title> About Us </title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="bootstrap.min.css">
+
+</head>
+
+<body>
+    <section class="about-us">
+        <div class="about">
+               <div class="hero-image">
+                <img style="float: right; margin: 0px 0px 15px 15px;" src="../sweproject1/slider-21.jpg" class="pic" >
+            </div>
+            <div class="text">
+                <div class="body2">
+                    <h5 ><span>About Us</span></h5>
+                    <h3>We guarantee you a comfortable travel experience
+                        Enjoyable with professional drivers</h3>
+                    <p>Do you want to enjoy all the benefits and luxury of a limousine, and travel in elegant and distinctive style?
+                        We guarantee you a comfortable and enjoyable travel experience with professional drivers and modern and luxurious vehicles
+                        
+                        We also provide customer service 24 hours a day, 7 days a week to ensure your comfort and the comfort of our customers
+                        
+                        24 Seven Limousine availability
+                        
+                        All types of luxury cars that suit all numbers of people to enjoy a quiet, safe and comfortable trip
+                        
+                        We guarantee that we have a vehicle to suit all your needs</p>
+                    <div class="data">
+                        <a href="#" class="hire">Book now
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    </center>
+    <script>
+        function hourglass() {
+          var a;
+          a = document.getElementById("div1");
+          a.innerHTML = "&#xf251;";
+          setTimeout(function () {
+              a.innerHTML = "&#xf252;";
+            }, 1000);
+          setTimeout(function () {
+              a.innerHTML = "&#xf253;";
+            }, 2000);
+        }
+        hourglass();
+        setInterval(hourglass,3000);
+        </script>
+</body>
+
+</html>

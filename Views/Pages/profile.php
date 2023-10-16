adf02937e6675fd7f4d42575427d63be6090cfd3
ranaslait <125487372+ranaslait@users.noreply.github.com>
2023-10-16 17:42:25 +0300

    userprofile

---
+++ Views/Pages/profile.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<head>      <link rel="icon" href="../../Public/Images/logodowns.png" type="image/gif">
+<link rel="stylesheet" href="../../Public/CSS/profile.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<body>
+ <?php
+       include('../../Views/Partials/sidebar.php');
+    ?> 
+<div class="main">
+   
+    </head>
+        <h2>PROFILE</h2>
+        <div class="card">
+            <div class="card-body">
+            <a class="button touch edit" href="../../Views/Pages/editProfile"></a>
+            <a class="button touch delete" href="#"></a>
+                <table>
+                    <tbody>
+                        <tr>
+                            <td>Name</td>
+                            <td>:</td>
+                            <td>ImDezCode</td>
+                        </tr>
+                        <tr>
+                            <td>Email</td>
+                            <td>:</td>
+                            <td >imdezcode@gmail.com</td>
+                        </tr>
+                        <tr>
+                            <td>Address</td>
+                            <td>:</td>
+                            <td>Bali, Indonesia</td>
+                        </tr>
+                        <tr>
+                            <td>Hobbies</td>
+                            <td>:</td>
+                            <td>Diving, Reading Book</td>
+                        </tr>
+                        <tr>
+                            <td>Job</td>
+                            <td>:</td>
+                            <td>Web Developer</td>
+                        </tr>
+                        <tr>
+                            <td>Skill</td>
+                            <td>:</td>
+                            <td>PHP, HTML, CSS, Java</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+
+       
+    </div>
+</body>
+</html>
--- Views/Partials/sidebar.php
+++ Views/Partials/sidebar.php
@@ -1,10 +1,6 @@
 <html lang="en">
     <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        
         <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,500;1,400;1,500&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="../../Public/CSS/sidebar.css">
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v6.3.0/css/all.css" >
@@ -28,7 +24,7 @@
                         </a>
                     </li>
                     <li class="menu-item">                        
-                        <a class="menu-link" href="#">
+                        <a class="menu-link" href="../../Views/Pages/profile.php">
                             <i class="fas fa-solid fa-user"></i>
                             <span class="menu-link-text">Profile</span>    
                         </a>

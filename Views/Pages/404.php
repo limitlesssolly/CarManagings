fc390babd59068b7acb25b27a54787b36d82d332
ranaslait <125487372+ranaslait@users.noreply.github.com>
2023-10-16 19:04:25 +0300

    404page

---
+++ Views/Pages/404.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+<link rel="icon" href="../../Public/Images/logodowns.png" type="image/gif">
+    <link rel="stylesheet" href="../../Public/CSS/404.css">
+</head>
+<p class="zoom-area"><b>Page</b> Not Found </p>
+<section class="error-container">
+  <span><span>4</span></span>
+  <span>0</span>
+  <span><span>4</span></span>
+</section>
+<div class="link-container">
+  <a target="_blank" href="../../index.php" class="more-link">Back to Home Page</a>
+</div>
+</html>

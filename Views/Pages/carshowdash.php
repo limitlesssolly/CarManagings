1bdb229dabc93c22b7e6129cb5ea4f1364d10cde
saheramr15 <125751958+saheramr15@users.noreply.github.com>
2023-10-17 20:39:08 +0300

    Create carshowdash.php

---
+++ Views/Pages/carshowdash.php
@@ -0,0 +1,145 @@
+
+ <!DOCTYPE html>
+<html lang="en" >
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Our Cars</title>
+    <link rel="stylesheet" href="../../Public/CSS/carshowdash.css">
+</head>
+
+<body>
+    <main class="table">
+        <section class="table__header">
+            <h1>Our Cars</h1>
+            <div class="input-group">
+                <input type="search" placeholder="Search Data...">
+                <img src="../../Public/Images/search.png" alt="">
+            </div>
+            <div class="export__file">
+                <label for="export-file" class="export__file-btn" title="Export File"></label>
+                <input type="checkbox" id="export-file">
+                <div class="export__file-options">
+                    <label>Export As &nbsp; &#10140;</label>
+                    <label for="export-file" id="toPDF">PDF <img src="images/pdf.png" alt=""></label>
+                    <label for="export-file" id="toJSON">JSON <img src="images/json.png" alt=""></label>
+                    <label for="export-file" id="toCSV">CSV <img src="images/csv.png" alt=""></label>
+                    <label for="export-file" id="toEXCEL">EXCEL <img src="images/excel.png" alt=""></label>
+                </div>
+            </div>
+        </section>
+        <section class="table__body">
+            <table>
+                <thead>
+                    <tr>
+                        <th> Id <span class="icon-arrow">&UpArrow;</span></th>
+                        <th> Name <span class="icon-arrow">&UpArrow;</span></th>
+                        <th> Type<span class="icon-arrow">&UpArrow;</span></th>
+                        <th> Number <span class="icon-arrow">&UpArrow;</span></th>
+                        <th> Status <span class="icon-arrow">&UpArrow;</span></th>
+                        <th> Total Profit <span class="icon-arrow">&UpArrow;</span></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td> 1 </td>
+                        <td> <img src="images/Zinzu Chan Lee.jpg" alt="">Toyota</td>
+                        <td> Sedan </td>
+                        <td> 123|ن ص ع </td>
+                        <td>
+                            <p class="status delivered"></p>
+                        </td>
+                        <td> <strong> $128.90 </strong></td>
+                    </tr>
+                    <tr>
+                        <td> 2 </td>
+                        <td><img src="images/Jeet Saru.jpg" alt=""> Toyota </td>
+                    
+                        <td>  </td>
+                        <td> 154|ت ص غ </td>
+                        <td>
+                            <p class="status cancelled">Cancelled</p>
+                        </td>
+                        <td> <strong>$5350.50</strong> </td>
+                    </tr>
+                    <tr>
+                        <td> 3</td>
+                        <td><img src="images/Sonal Gharti.jpg" alt=""> Sonal Gharti </td>
+                        <td> Tokyo </td>
+                        <td> 154|ت ص غ </td>
+                        <td>
+                            <p class="status shipped">Shipped</p>
+                        </td>
+                        <td> <strong>$210.40</strong> </td>
+                    </tr>
+                    <tr>
+                        <td> 4</td>
+                        <td><img src="images/Alson GC.jpg" alt=""> Alson GC </td>
+                        <td> New Delhi </td>
+                        <td> 154|ت ص غ </td>
+                        <td>
+                            <p class="status delivered">Delivered</p>
+                        </td>
+                        <td> <strong>$149.70</strong> </td>
+                    </tr>
+                    <tr>
+                        <td> 5</td>
+                        <td><img src="images/Sarita Limbu.jpg" alt=""> Sarita Limbu </td>
+                        <td> Paris </td>
+                        <td> 154|ت ص غ </td>
+                        <td>
+                            <p class="status pending">Pending</p>
+                        </td>
+                        <td> <strong>$399.99</strong> </td>
+                    </tr>
+                    <tr>
+                        <td> 6</td>
+                        <td><img src="images/Alex Gonley.jpg" alt=""> Hai S </td>
+                        <td> van </td>
+                        <td> 154|ت ص غ </td>
+                        <td>
+                            <p class="status cancelled">Cancelled</p>
+                        </td>
+                        <td> <strong>$399.99</strong> </td>
+                    </tr>
+                    <tr>
+                        <td> 7</td>
+                        <td><img src="images/Alson GC.jpg" alt=""> Hyundai </td>
+                        <td> sedan </td>
+                        <td> 154|ت ص غ </td>
+                        <td>
+                            <p class="status delivered">Delivered</p>
+                        </td>
+                        <td> <strong>$399.99</strong> </td>
+                    </tr>
+                    <tr>
+                        <td> 8</td>
+                        <td><img src="images/Sarita Limbu.jpg" alt=""> Nissan </td>
+                        <td> hatchback </td>
+                        <td> 154|ت ص غ </td>
+                        <td>
+                            <p class="status pending">Pending</p>
+                        </td>
+                        <td> <strong>$149.70</strong> </td>
+                    </tr>
+                    <tr>
+                        <td> 9</td>
+                        <td><img src="images/Alex Gonley.jpg" alt=""> Hyundai </td>
+                        <td> Suv </td>
+                        <td> 154|ت ص غ </td>
+                        <td>
+                            <p class="status cancelled">Cancelled</p>
+                        </td>
+                        <td> <strong>$249.99</strong> </td>
+                    </tr>
+                </tbody>
+                <script src="../../Public/js/carshowdash.js"></script>
+            </table>
+            
+        </section>
+    </main>
+</body>
+
+</html>

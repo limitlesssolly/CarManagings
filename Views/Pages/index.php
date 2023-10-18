4b6b4b81d34aa2f03eadcf3ccdac867302f87ade
saheramr15 <125751958+saheramr15@users.noreply.github.com>
2023-10-18 15:07:04 +0300

    Create index.php

---
+++ Views/Pages/index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../Public/CSS/footer2.css">
+    <link rel="stylesheet" href="../../Public/CSS/index.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      rel="stylesheet">
+    <link rel="stylesheet" href="../../Public/CSS/navbar2.css">
+    <script src="https://code.jquery.com/jquery-3.7.0.js" integrity="sha256-JlqSTELeR4TLqP0OG9dxM7yDPqX1ox/HfgiSLBj8+kM=" crossorigin="anonymous"></script>
+
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'>                      
+
+   
+    <title>Home</title>
+</head>
+<body>
+    
+        <?php
+            include('../Partials/navbar2.php');
+        ?>
+        
+        <div class="bodyimage">
+            <img src="../../Public/Images/carbackground.jpg "alt="">
+        </div>
+        <section id="introo">
+            <div class="intro ">
+            <p class="p1">24Seven limousine for best travelling</p>
+            <p class="p2"> Established in 2020 By a Group of <br> Challengers from Different Fields</p>
+            </div>
+        </section>      
+        
+        
+        <div class="container3">
+               <div class="box2">
+                    <img src="../../Public/Images/mitsubishilogo.png" alt="">
+                </div>
+
+                <div class="box2">
+                    <img src="../../Public/Images/toyotalogo.png" alt="" id="toyota">
+                </div>
+
+                <div class="box2">
+                    <img src="../../Public/Images/hyundai-logo.png" alt="">
+                </div>
+        </div>
+        
+        <?php
+            include('../Partials/footer2.php');
+        ?>
+
+
+</body>
+</html>

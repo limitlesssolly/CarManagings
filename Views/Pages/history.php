44eaeed7f338a732e69d323567522b592315566b
ranaslait <125487372+ranaslait@users.noreply.github.com>
2023-10-22 00:49:35 +0300

    historyedit

--- Views/Pages/history.php
+++ Views/Pages/history.php
@@ -2,26 +2,22 @@
 <html lang="en">
 <head>      <link rel="icon" href="../../Public/Images/logodowns.png" type="image/gif">
 <link rel="stylesheet" href="../../Public/CSS/history.css">
-
 </head>
 <body style="background-color: #ffffff;">
 <?php
-       include('../../Views/Partials/sidebar.php');
+      include('../../Views/Partials/sidebar.php');
     ?> 
 <div class="center">
   <ul>
     <li>
       <div class="image">
         <img src="https://i.discogs.com/Vi5Q2RPLp3QM2zpp6_ANS1wGgB-rfy4J2Aj1KI9ItMs/rs:fit/g:sm/q:90/h:500/w:500/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9BLTEwOTUy/MTAtMTYzNjE0NTAw/NS0zNzE5LmpwZWc.jpeg" alt="">
-        
         <h4>Driver 1</h4>
       </div>
-      
       <div class="text-group">
       <div style="font-size: 20px;"> Start Point:Alex</div>
       <div style="font-size: 20px;"> Destination:Cairo</div>
       </div>
-      
       <div class="date-group">
         <h4>March 13, 2023</h4>
         <p>5:00 PM</p>
@@ -32,19 +28,15 @@
   </a>
 </div>
     </li>
-
     <li>
       <div class="image">
         <img src="https://i.discogs.com/Vi5Q2RPLp3QM2zpp6_ANS1wGgB-rfy4J2Aj1KI9ItMs/rs:fit/g:sm/q:90/h:500/w:500/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9BLTEwOTUy/MTAtMTYzNjE0NTAw/NS0zNzE5LmpwZWc.jpeg" alt="">
-        
         <h4>Driver 2</h4>
       </div>
-      
       <div class="text-group">
       <div style="font-size: 20px;"> Start Point:Alex</div>
       <div style="font-size: 20px;"> Destination:Cairo</div>
       </div>
-      
       <div class="date-group">
         <h4>March 13, 2023</h4>
         <p>5:00 PM</p>
@@ -58,15 +50,12 @@
     <li>
       <div class="image">
         <img src="https://i.discogs.com/Vi5Q2RPLp3QM2zpp6_ANS1wGgB-rfy4J2Aj1KI9ItMs/rs:fit/g:sm/q:90/h:500/w:500/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9BLTEwOTUy/MTAtMTYzNjE0NTAw/NS0zNzE5LmpwZWc.jpeg" alt="">
-        
         <h4>Driver 3</h4>
-      </div>
-      
+      </div>      
       <div class="text-group">
       <div style="font-size: 20px;"> Start Point:Alex</div>
       <div style="font-size: 20px;"> Destination:Cairo</div>
-      </div>
-      
+      </div>      
       <div class="date-group">
         <h4>March 13, 2023</h4>
         <p>5:00 PM</p>

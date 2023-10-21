29f48fcc8e47f530cf28d8b1bcf15f5231cfa02c
saheramr15 <125751958+saheramr15@users.noreply.github.com>
2023-10-21 21:03:20 +0300

    Create editcustomers.php

---
+++ Views/Pages/editcustomers.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../Public/CSS/editprofiledash.css">
+    <link rel="stylesheet" href="../../Public/CSS/dashboard.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+
+    <title>Edit customer</title>
+</head>
+<body>
+        <?php
+          include('../Partials/dashboardsidebar.php');
+        ?> 
+        <section class="home-section">
+            <div class="home-content">
+                <i class="bx bx-menu"></i>
+                <span class="text">Dashboard</span>
+            </div>
+            <!-- form -->
+            <div class="contactForm">
+                    <h2>Updating info</h2>
+                    <form  id="contactForm">
+
+                        <div class="inputBox">
+                            <input type="text" name="name"  id="name" placeholder="please enter the id of the customer">
+                            <span>Id</span>
+                        </div>
+
+                        <div class="inputBox">
+                            <input type="text" name="email" id="email">
+                            <span>Name</span>
+                        </div>
+
+                        <div class="inputBox">
+                            <input type="text" name="email" id="address">
+                            <span>Email</span>
+                        </div>
+
+
+                        <div class="inputBox">
+                            <input type="text" name="phone" id="phone" >
+                            <span>Phone</span>
+                        </div>
+
+
+                     
+                        <div id="error"> </div>
+
+                      
+
+                        <div class="inputBox">
+                            <input type="submit" value="SEND">
+                        </div>
+                        
+                    </form>
+                    <!-- <script src="js/contactus.js"></script> -->
+                </div>
+<!-- end of form -->
+        </section>    
+        <script>
+            let arrow = document.querySelectorAll(".arrow");
+            for(var i=0;i<arrow.length;i++){
+                arrow[i].addEventListener("click",(e)=>{
+                    let arrowParent=e.target.parentElement.parentElement;
+                    console.log(arrowParent)
+                    arrowParent.classList.toggle("showMenu");
+                });
+            }
+            let sidebar=document.querySelector(".sidebar");
+            let sidebarBtn=document.querySelector(".bx-menu");
+            sidebarBtn.addEventListener("click",()=>{
+                sidebar.classList.toggle("close")
+            })
+
+
+        </script>
+
+                
+
+
+
+</body>
+</html>

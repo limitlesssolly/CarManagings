a0bed1541f6baa747f599cd29317cb3887b3c95d
shahd ahmed <shahdahmed7203@gmail.com>
2023-10-23 16:11:20 +0300

    code el delete

--- includes/db.php
+++ includes/db.php
@@ -11,3 +11,11 @@ if(!$conn){
     die("Connection failed: " . mysqli_connect_error());
 }
 ?>
+
+<!-- $sql = "DELETE FROM drivers WHERE fullname = $fullname";
+
+if ($conn->query($sql) === TRUE) {
+  echo "Record deleted successfully";
+} else {
+  echo "Error deleting record: " . $conn->error;
+} -->

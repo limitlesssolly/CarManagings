a7909e115dd04e7b3821b3933dcaf60c32ba48cf
saheramr15 <125751958+saheramr15@users.noreply.github.com>
2023-10-25 00:50:18 +0300

    Update ratingdash.php

--- Views/Pages/ratingdash.php
+++ Views/Pages/ratingdash.php
@@ -1,13 +1,66 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+<!-- Coding By CodingNepal - codingnepalweb.com -->
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title> Responsive Login and Signup Form </title>
 
+        <!-- CSS -->
+        <link rel="stylesheet" href="../../Public/CSS/ratingdash.css">
+                
+        <!-- Boxicons CSS -->
+        <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'>
+                        
+    </head>
+    <body>
+    <?php
+          include('../Partials/dashboardsidebar.php');
+        ?> 
+    <section class="home-section">
+        <div class="home-content">
+            <i class="bx bx-menu"></i>
+            <span class="text">Dashboard</span>
+        </div>
+        <div class="main-container">
+        <div class="heading">
+        <!-- <h1 class="heading__title">Ratings</h1>  -->
+        </div>
+        <div class="cards">
+        <div class="card card-1">
+            
+            <div class="card__icon"><p>From:saher@gmail.com <br> at:12/12/202</p></div>
+            
+            
+            <p class="card__exit"><i class="fas fa-times"></i></p>
+            <h2 class="card__title">It was a very safe drive.</h2>
+            
+        </div>
+        <div class="card card-1">
+            <div class="card__icon"><p>From:saher@gmail.com <br> at:12/12/202</p></div>
+            <p class="card__exit"><i class="fas fa-times"></i></p>
+            <h2 class="card__title">I want to thank you for your nice service .</h2>
+            
+        </div>
+        <div class="card card-1">
+            <div class="card__icon"><p>From:saher@gmail.com <br> at:12/12/202</p></div>
+            <p class="card__exit"><i class="fas fa-times"></i></p>
+            <h2 class="card__title">the driver was very helpful.</h2>
+            
+        </div>
+        <div class="card card-1">
+            <div class="card__icon"><p>From:saher@gmail.com <br> at:12/12/202</p></div>
+            <p class="card__exit"><i class="fas fa-times"></i></p>
+            <h2 class="card__title">I had a problem with fast driving.</h2>
+            
+        </div>
+      
+    </div>
+  </div>
 
-    
-</body>
+    </section>
+
+  
+    </body>
 </html>

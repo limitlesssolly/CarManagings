d0a4a86e463948f17a8fd7a18710e9496d34f0cc
saheramr15 <125751958+saheramr15@users.noreply.github.com>
2023-12-05 20:25:13 +0200

    Create rides.php

---
+++ App/Models/rides.php
@@ -0,0 +1,51 @@
+
+<?php
+
+include_once "../App/Database/db.php";
+ 
+class Rides{
+
+   public $ID;
+   public $PickupDate;
+   public $pickupTime;
+   public $pickupLocation;
+   public $pickupDestination;
+   public $Status;
+   public $CarPlate;
+   public $CarType;
+   public $DriverID;
+   public $UserName;
+   public $UserEmail;
+   public $UserPhone;
+   public $Comment;
+   Public $PaymentWay;
+   Public $TotalCost;
+
+   Public function InsertInDB($PickupDate,$pickupTime,$pickupLocation,$pickupDestination,$Status,$CarType,$UserName,$UserEmail,$UserPhone,$Comment,$PaymentWay,$TotalCost){
+    $sql = "insert into rides(PickupDate,pickupTime,pickupLocation,pickupDestination,Status,CarPlate,CarType,DriverID,UserName,UserEmail,UserPhone,Comment,PaymentWay,TotalCost) 
+    values('$PickupDate','$pickupTime','$pickupLocation', '$pickupDestination','$Status','null',
+    '$CarType','null','$UserName','$UserEmail','$UserPhone','$Comment','$PaymentWay','$TotalCost')";
+    $result = mysqli_query($GLOBALS['conn'], $sql);
+    if ($result) {
+        return 0;
+     }
+     else{
+        echo "error";
+     }
+   }  
+
+
+   static function AssignDriverandCar($rideID,$DriverID,$CarID){
+    $query = "SELECT * FROM drivers where ID='$DriverID'";
+    $query2 = "SELECT * FROM cars where ID='$CarID'";
+   }
+   static function GetAllRides(){
+      $sql = "SELECT * FROM rides";
+      $result = mysqli_query($GLOBALS['conn'], $sql);
+      return $result;
+   }
+
+
+}
+
+?>

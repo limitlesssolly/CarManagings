dccb6f7926e2dbe56c95271cf790d591e2991717
saheramr15 <125751958+saheramr15@users.noreply.github.com>
2023-12-06 03:09:02 +0200

    Update DriversControllers.php

--- App/Controllers/DriversControllers.php
+++ App/Controllers/DriversControllers.php
@@ -1,19 +1,76 @@
 <?php
 
 require "Controller.php";
-
+require_once("../App/Models/drivers.php");
 class DriversController extends Controller{
-	public function add() {
-		$name = $_REQUEST['name'];
-		$email = $_REQUEST['email'];
-		$phone = $_REQUEST['phone'];
-		$date = $_REQUEST['date'];
-		$status = $_REQUEST['status'];
+	public function Add($name,$email,$phone,$date,$img,$status) {
 
-		$this->model->addDriver($name,$email,$phone,$date,$status);
+		$error=[
+            'name'=>'',
+            'email'=>'',
+            'phone'=>'',
+            'date'=>'',
+			'img'=>'',
+			'status'=>''
+        ];
+        $i=0;
+        if($name =='')
+        {
+            $i++;
+            $error['name']='please enter name';
+        }
+
+        if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            
+        } else {
+            $i++;
+            $error['email']='please enter a valid email address';
+        }
+
+        $string = $phone;
+        $phonel='';
+        for ($y = 0; $y < strlen($string); $y++) {
+            if (is_numeric($string[$i])) {
+                $phonel .= $string[$i];
+            }
+        }  
+
+        if(strlen($phonel) !=11 )
+        {
+            $i++;
+            $error['phone']='please enter a valid phone number';
+        }
+		// $this->model->addDriver($name,$email,$phone,$date,$status);
+        if($date=='')
+		{
+			$i++;
+            $error['date']='please enter a date';
+		}
+		if($img=='')
+		{
+			$i++;
+            $error['img']='please enter a photo';
+		}    
+        if($status=='')
+		{
+			$i++;
+            $error['status']='please enter status';
+		} 
+
+
+		if($i==0)
+		{
+			$drivers=new  Driver();
+			$drivers->addDriver($name,$email,$phone,$date,$status,$img);
+			return 'successful';
+		}else{
+			return $error;
+		}
+
 	}
 	public function getdrivers(){
-	 return	$this->model->readDrivers();
+		$drivers=new  Driver();
+	   return	$drivers->readDrivers();
 	}
 
 
@@ -28,16 +85,5 @@ class DriversController extends Controller{
 	
 	public function delete(){
 		$this->model->removeDrivers();
-	}
-	public function allDrivers(){
-		$str="";
-		$str.=$this->model->getName();
-		$str.=$this->model->getEmail();
-		$str.=$this->model->getPhone();
-		$str.=$this->model->getDate();
-		$str.=$this->model->getStatus();
-		// $str.="<h5>".$this->model->getStatus()."</h5>";
-		// $str.="<h5>".$this->model->getStatus()."</h5>";
-		return $str;
 	}
 }

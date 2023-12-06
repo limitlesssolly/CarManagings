c2ba4d2c50ceb8cc236c914e9bee152aa9055d33
saheramr15 <125751958+saheramr15@users.noreply.github.com>
2023-12-07 00:16:58 +0200

    Update editdriver.js

--- Public/js/editdriver.js
+++ Public/js/editdriver.js
@@ -1,18 +1,4 @@
-// $(document).ready(function() {
-//     $("#email").keyup(function() {
-//       var email = $("#email").val();
-//       const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-//       if (emailRegex.test(email)) {
-//         $("#Validation").html("");
-// 		document.getElementById('sign').disabled = false;
-//       } else {
-//         $("#Validation").html("Please enter a valid email").css("color", "red");
-// 		document.getElementById('sign').disabled = true;		
-//       }
-//     });
-//   });
 
-console.log('yes')
 $(document).ready(function() {
   $("Form").submit(function(event) {
      event.preventDefault();

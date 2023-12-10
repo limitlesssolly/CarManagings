f7659b9c34ff0205ed7343b4c3be28d44fa0a362
MamdouhShawer <shawermamdouh4@gmail.com>
2023-12-10 16:51:43 +0200

    adding car routers

--- App/Controllers/CarsControllers.php
+++ App/Controllers/CarsControllers.php
@@ -1,28 +1,56 @@
 <?php
 
-require_once("../Models/cars.php");
+require "Controller.php";
+require_once("../models/cars.php");
+class CarController extends Controller {
+    public function Add($id, $model, $year, $color, $fuelType) {
+        try {
+            // Validate input parameters
+            $this->validateInput($id, $model, $year, $color, $fuelType);
 
-$car = new Car();
+            // Perform the actual operation (e.g., add a car)
+            $this->performAddOperation($id, $model, $year, $color, $fuelType);
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $model = htmlspecialchars($_POST["model"]);
-    $year = htmlspecialchars($_POST["year"]);
-    $color = htmlspecialchars($_POST["color"]);
-    $fuelType = htmlspecialchars($_POST["fuelType"]);
-}
+            // Respond with success message or data
+            return ['success' => true, 'message' => 'Car added successfully'];
+        } catch (Exception $e) {
+            // Handle errors
+            return ['success' => false, 'message' => $e->getMessage()];
+        }
+    }
 
-if (!empty($_GET["action"])) {
-    switch ($_GET["action"]) {
-        case "add":
-            if (!empty($_POST["id"])) {
-                $car->addCar($_GET["id"], $model, $year, $color, $fuelType);
-            }
-            break;
-        case "remove":
-            $car->removeCar($_GET["id"]);
-            break;
+    private function validateInput($id, $model, $year, $color, $fuelType) {
+         // Placeholder for database connection (replace with your actual database connection logic)
+        $dbConnection = new PDO('mysql:host=localhost;dbname=your_database', 'username', 'password');
+
+        // Placeholder SQL query (replace with your
+
+        if (empty($id) || empty($model) || empty($year) || empty($color) || empty($fuelType)) {
+            throw new Exception('All fields are required');
+        }
+
+        // Additional validation logic can be added based on your requirements
+    }
+
+    private function performAddOperation($id, $model, $year, $color, $fuelType) {
+        // Perform the actual operation (e.g., adding a car to the database)
+        // You can add your database logic or any other business logic here
+        // If an error occurs during the operation, throw an exception
+        // For example:
+        // $result = $database->insertCar($id, $model, $year, $color, $fuelType);
+        // if (!$result) {
+        //     throw new Exception('Failed to add car');
+        // }
     }
 }
 
+// Example usage:
+$carController = new CarController();
+$result = $carController->Add($id, $model, $year, $color, $fuelType);
 
-?>
+// Check the result and handle accordingly
+if ($result['success']) {
+    echo $result['message'];
+} else {
+    echo 'Error: ' . $result['message'];
+}

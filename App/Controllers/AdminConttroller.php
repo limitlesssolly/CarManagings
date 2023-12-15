0d8920a98779e14aa330d1fd1b7ada0ba19badba
saheramr15 <125751958+saheramr15@users.noreply.github.com>
2023-12-15 02:49:40 +0200

    Update AdminConttroller.php

--- App/Controllers/AdminConttroller.php
+++ App/Controllers/AdminConttroller.php
@@ -1,72 +1,181 @@
 <?php
 
-require_once 'admins.php';
+require "Controller.php";
+require_once("../App/Models/admins.php");
 
-class AdminController
-{
-    private $adminModel;
+class AdminController extends Controller{
 
-    public function __construct()
-    {
-        $this->adminModel = new AdminModel();
-    }
+   public $admin;
+   public function __construct() {
+    $this->admin=new  Admin();
+   }
 
-    public function index()
-    {
-        // Display a list of cars
-        $cars = $this->adminModel->getAllCars();
-        include 'views/admin/cars/index.php';
-    }
+    public function Add($name,$email,$phone,$pass,$confirmpass) {
+		$error=[
+            'name'=>'',
+            'email'=>'',
+            'phone'=>'',
+            'pass'=>'',
+            'confirmpass'=>'',
+        ];
+        $i=0;
+        if($name =='')
+        {
+            $i++;
+            $error['name']='please enter name';
+        }
+        if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
 
-    public function show($carId)
-    {
-        // Display details of a specific car
-        $car = $this->adminModel->getCarById($carId);
-        include 'views/admin/cars/show.php'; 
-    }
+            $result= $this->admin->if_Emailexists($email);
+            if($result=='exists'){
+                $i++;
+                $error['email']='please choose antoher email address'; 
+            }
 
-    public function create()
-    {
-        // Show the form for adding a new car
-        include 'views/admin/cars/create.php'; 
-    }
+        } else {
+            $i++;
+            $error['email']='please enter a valid email address';
+        }
+        $string = $phone;
+        $phonel='';
+        for ($y = 0; $y < strlen($string); $y++) {
+            if (is_numeric($string[$i])) {
+                $phonel .= $string[$i];
+            }
+        }  
+        if(strlen($phonel) !=11 )
+        {
+            $i++;
+            $error['phone']='please enter a valid phone number';
+        }
 
-    public function store()
-    {
-        // Handle the form submission for adding a new car
-        $carDetails = $_POST; // Assuming you're using POST data
-        $this->adminModel->addCar($carDetails);
+        if($pass=='')
+        {
+            $i++;
+            $error['pass']='please enter a password';
+        }else {
+            $uppercase = preg_match('@[A-Z]@', $pass);
+            $lowercase = preg_match('@[a-z]@', $pass);
+            $number = preg_match('@[0-9]@', $pass);
+            
+            // Define minimum length for the password
+            $minLength = 8;
+            
+            if ($uppercase && $lowercase && $number && strlen($pass) >= $minLength) {
+                
+                if($confirmpass=='')
+                {
+                    $i++;
+                    $error['confirmpass']='please enter a confirmation password';
+                } 
+                else if($confirmpass!=$pass){
+                    $i++;
+                    $error['confirmpass']="passwords dont match";
+                }
+                     
+            } else {
+                $i++;
+                $error['pass']='password must be at least 8 characters including uppercase,lowrcase,and numbers';
 
-        // Redirect to the list of cars or show a success message
-        header('Location: /admin/cars'); 
-        exit;
-    }
+                if($confirmpass=='')
+                {
+                    $i++;
+                    $error['confirmpass']='please enter a confirmation password';
+                } 
+                else{
+                    $i++;
+                    $error['confirmpass']="please a valid password first";
+                }
+            }
+        }
 
-    public function edit($carId)
-    {
-        // Show the form for editing a car
-        $car = $this->adminModel->getCarById($carId);
-        include 'views/admin/cars/edit.php';
-    }
+		if($i==0)
+		{
+            $hashedPassword = password_hash($pass, PASSWORD_DEFAULT);
+			$this->admin->AddAdmin($name,$email,$phone,$hashedPassword);
+			return 'successful';
+		}else{
+			return $error;
+		}
+	}
+	public function getAdmins(){
 
-    public function update($carId)
-    {
-        // Handle the form submission for updating a car
-        $newDetails = $_POST; // Assuming you're using POST data
-        $this->adminModel->updateCar($carId, $newDetails);
+	   return  $this->admin->getAdmins();
+	}
 
-        // Redirect to the list of cars or show a success message
-        header('Location: /admin/cars'); 
-        exit;
-    }
+	public function Delete($id){
 
-    public function delete($carId)
-    {
-        // Delete a car
-        $this->adminModel->deleteCar($carId);
+		return$this->admin->removeAdmin($id);
+	}
+	public function Edit($id,$name,$email,$phone,$pass,$confirmpass){
+		 $i=0;
+         if($id == null)
+		 {
+            $i++;
+			return 'please enter id';
 
-        // Redirect to the list of cars or show a success message
-        header('Location: /admin/cars');
-        exit;
-    }
+		 }else{
+
+		$driverinfo =$this->admin->readAdmin($id);
+		while ($row = $driverinfo->fetch_assoc()) {
+			if($name==''){
+               $name=$row['Name'];
+			}
+
+			if($email==''){
+				$email=$row['Email'];
+			 }
+
+			 if($phone==''){
+				$phone=$row['Phone'];
+			 }
+                 
+             if($pass=='' && $confirmpass== ''){
+				$pass=$row['Password'];
+			 }                   
+               
+             if($pass!=''){
+                $uppercase = preg_match('@[A-Z]@', $pass);
+                $lowercase = preg_match('@[a-z]@', $pass);
+                $number = preg_match('@[0-9]@', $pass);
+                
+                $minLength = 8;
+                
+                if ($uppercase && $lowercase && $number && strlen($pass) >= $minLength) {
+                    
+                    if($confirmpass=='')
+                    {
+                        $i++;
+                        $error['confirmpass']='please enter a confirmation password';
+                    } 
+                    else if($confirmpass!=$pass){
+                        $i++;
+                        $error['confirmpass']="passwords dont match";
+                    }
+                         
+                } else {
+                    $i++;
+                    $error['pass']='password must be at least 8 characters including uppercase,lowrcase,and numbers';
+    
+                    if($confirmpass=='')
+                    {
+                        $i++;
+                        $error['confirmpass']='please enter a confirmation password';
+                    } 
+                    else{
+                        $i++;
+                        $error['confirmpass']="please a valid password first";
+                    }
+                }         
+             }
+               
+			 $this->admin->editAdmin($id,$name,$email,$phone,$pass);
+			 return	'successful';
+		   } 
+		 }  
+
+
+
+
+	}
 }

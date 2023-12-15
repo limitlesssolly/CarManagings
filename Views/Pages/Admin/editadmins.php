2e59ab7e7b13edd8019c64660d805b5c7f09ed6e
saheramr15 <125751958+saheramr15@users.noreply.github.com>
2023-12-15 02:49:05 +0200

    Update editadmins.php

--- Views/Pages/Admin/editadmins.php
+++ Views/Pages/Admin/editadmins.php
@@ -6,8 +6,19 @@
     <link rel="stylesheet" href="../../../Public/CSS/editprofiledash.css">
     <link rel="stylesheet" href="../../../Public/CSS/dashboard.css">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
-
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="../../../Public/CSS/alert.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <title>Edit admin</title>
+    <style>
+        body{
+            overflow: hidden;
+        }
+        .error{
+            color: red;
+        }
+    </style>
 </head>
 <body>
         <?php
@@ -18,36 +29,62 @@
                 <i class="bx bx-menu"></i>
                 <span class="text">Dashboard</span>
             </div>
+            <div class="alert hide"> 
+                        <span class="fas fa-check-circle"></span>
+                        <span class="msg">Edited Successfully!</span>
+                        <div class="close-btn">
+                            <span class="fas fa-times"></span>
+                        </div>
+        </div>
             <!-- form -->
             <div class="contactForm">
                     <h2>Updating info</h2>
                     <form  id="contactForm">
 
                 
-
+                    <div class="inputBox">
+                            <input type="text" name="id" id="id"  required>
+                            <span>ID</span>
+                            <p class="error" id='iderror'></p>
+                        </div>
                         <div class="inputBox">
                             <input type="email" name="email" id="email"  required>
                             <span>Email</span>
+                            <p class="error" id='emailerror'></p>
                         </div>
 
                         <div class="inputBox">
-                            <input type="text" name="email" id="address" required>
+                            <input type="text" name="name" id="address" required>
                             <span>Name</span>
+                            <p class="error" id='nameerror'></p>
                         </div>
 
                         <div class="inputBox">
                             <input type="text" name="phone" id="phone" required>
                             <span>Phone</span>
+                            <p class="error" id='phoneerror'></p>
+                        </div>
+
+                        <div class="inputBox">
+                            <input type="password" name="pass" id="pass"  required>
+                            <span>New Password</span>
+                            <p class="error" id='passerror'></p>
+                        </div>
+
+                        <div class="inputBox">
+                            <input type="password" name="confirmpass" id="confirmpass"  required>
+                            <span>Confirm New Password</span>
+                            <p class="error" id='confirmpasserror'></p>
                         </div>
 
                         <div id="error"> </div>
 
                         <div class="inputBox">
                             <input type="submit" value="SEND">
-                        </div>
+                        </div>    
                         
                     </form>
-                    <!-- <script src="js/contactus.js"></script> -->
+                    <script src="../../../Public/js/editadmin.js"></script>
                 </div>
 <!-- end of form -->
         </section>    

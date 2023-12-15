62530d235a55c6d05d3510a7d5e99656e283c8dd
saheramr15 <125751958+saheramr15@users.noreply.github.com>
2023-12-15 02:49:34 +0200

    Update db.php

--- App/Database/db.php
+++ App/Database/db.php
@@ -3,10 +3,7 @@ $servername = "localhost";
 $username = "root";
 $password = "";
 $DB = "24sevenlimousine";
-
 $conn = mysqli_connect($servername,$username,$password,$DB);
-
-
 if(!$conn){
     die("Connection failed: " . mysqli_connect_error());
 }

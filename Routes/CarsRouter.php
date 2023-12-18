af522e4a8430df00227bd9585da40e26456000b9
saheramr15 <125751958+saheramr15@users.noreply.github.com>
2023-12-19 01:16:46 +0200

    Update CarsRouter.php

--- Routes/CarsRouter.php
+++ Routes/CarsRouter.php
@@ -26,26 +26,41 @@ if ($_GET['action'] == 'cars') {
 } else if ($_GET['action'] == 'addcarpage') {
     header("Location: ../Views/Pages/Admin/addcars.php");
 } else if ($_GET['action'] == 'addcar') {
-    $result = $carController->Add($_POST['id'], $_POST['model'], $_POST['year'], $_POST['color'], $_POST['fuelType']);
-    if ($result['success']) {
+
+    
+    $details=[
+        'name'=>$_POST['name'],
+        'type'=>$_POST['type'],
+        'plate'=>$_POST['plate'],
+        'color'=>$_POST['color'],
+        'status'=>$_POST['status']
+    ];
+    
+    $result = $carController->addCar($details);
+     
+    if ($result=='successful') {
+
         echo 'successful';
     } else {
         echo json_encode($result);
     }
-} else if ($_GET['action'] == 'editcarpage') {
-    header("Location: ../Views/Pages/Admin/editcars.php");
-} else if ($_GET['action'] == 'editcar') {
-    $result = $carController->Edit($_POST['id'], $_POST['model'], $_POST['year'], $_POST['color'], $_POST['fuelType']);
-    if ($result['success']) {
-        echo 'successful';
-    } else {
-        echo json_encode($result);
-    }
-} else if ($_GET['action'] == 'deletecar') {
-    $result = $carController->Delete($_POST['id']);
-    if ($result['success']) {
-        echo 'successful';
-    } else {
-        echo 'failed';
-    }
-}
+
+ }
+
+//  else if ($_GET['action'] == 'editcarpage') {
+//     header("Location: ../Views/Pages/Admin/editcars.php");
+// } else if ($_GET['action'] == 'editcar') {
+//     $result = $carController->Edit($_POST['id'], $_POST['model'], $_POST['year'], $_POST['color'], $_POST['fuelType']);
+//     if ($result['success']) {
+//         echo 'successful';
+//     } else {
+//         echo json_encode($result);
+//     }
+// } else if ($_GET['action'] == 'deletecar') {
+//     $result = $carController->Delete($_POST['id']);
+//     if ($result['success']) {
+//         echo 'successful';
+//     } else {
+//         echo 'failed';
+//     }
+// }

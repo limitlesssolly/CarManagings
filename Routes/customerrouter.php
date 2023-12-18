6d823e0cf34f7276afe336779da849b70a2293c8
karim-7423 <karim2110342@miuegypt.edu.eg>
2023-12-18 22:28:03 +0200

    customer router is added

---
+++ Routes/customerrouter.php
@@ -0,0 +1,45 @@
+<?php
+session_start();
+require_once("../App/Controllers/CustomerController.php");
+
+$customer = new CustomerController();
+
+if ($_GET['action'] == 'customers') {
+    $result = $customer->getCustomers();
+    $_SESSION['AllCustomers'] = array();
+    $i = 0;
+    while ($row = $result->fetch_assoc()) {
+        $_SESSION['AllCustomers'][$i] = [
+            'ID' => $row['ID'],
+            'Name' => $row['Name'],
+            'Email' => $row['Email'],
+            'Phone' => $row['Phone'],
+        ];
+        $i++;
+    }
+    header("Location:../Views/Pages/Customer/customers.php");
+} else if ($_GET['action'] == 'addcustomerpage') {
+    header("Location:../Views/Pages/Customer/addcustomer.php");
+} else if ($_GET['action'] == 'addcustomer') {
+    $result = $customer->Add($_POST['name'], $_POST['email'], $_POST['phone'], $_POST['pass'], $_POST['confirmpass']);
+    if ($result == 'successful') {
+        echo 'successful';
+    } else {
+        echo json_encode($result);
+    }
+} else if ($_GET['action'] == 'deletecustomer') {
+    $result = $customer->Delete($_POST['id']);
+    if ($result == 'successful') {
+        echo 'successful';
+    } else {
+        echo 'failed';
+    }
+} else if ($_GET['action'] == 'editcustomerpage') {
+    header("Location:../Views/Pages/Customer/editcustomer.php");
+} else if ($_GET['action'] == 'editcustomer') {
+    // Handle editing customer data
+    // ...
+}
+
+
+?>
